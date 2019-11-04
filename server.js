54acf3217ae28630233cda740757789c10a23c1d
MIRABO <mirabo@MIRABOnoMacBook-Pro.local>
2019-11-04 10:28:02 +0700

    resfulapi with nodejs

---
+++ server.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+  
+  
+// default route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello' })
+});
+// connection configurations
+var dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'node_api',
+    port : 8889
+});
+  
+// connect to database
+dbConn.connect(); 
+ 
+ 
+// Retrieve all users 
+app.get('/user', function (req, res) {
+    dbConn.query('SELECT * FROM users', function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'users list.' });
+    });
+});
+ 
+ 
+// Retrieve user with id 
+app.get('/user/:id', function (req, res) {
+  
+    let user_id = req.params.id;
+  
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+  
+    dbConn.query('SELECT * FROM users where id=?', user_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'users list.' });
+    });
+  
+});
+ 
+ 
+// Add a new user  
+app.post('/user', function (req, res) {
+    
+    let id = req.body.id;
+    let name = req.body.name;
+    let email = req.body.email;
+
+
+    let sql = `INSERT INTO users (id, name,email) VALUES ("${id}","${name}","${email}")`
+    dbConn.query(sql, (error, results) =>{
+        if (error) throw error;
+        return res.send({ error: false, id: results.id, message: 'New user has been created successfully.' });
+    });
+});
+ 
+ 
+//  Update user with id
+app.put('/user', (req, res) => {
+  
+    let user_id = req.body.user_id;
+    let user = req.body.user;
+  
+    if (!user_id || !user) {
+        return res.status(400).send({ error: user, message: 'Please provide user and user_id' });
+    }
+  
+    dbConn.query("UPDATE users SET user = ? WHERE id = ?", [user, user_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'user has been updated successfully.' });
+    });
+});
+ 
+ 
+//  Delete user
+app.delete('/user', function (req, res) {
+  
+    let user_id = req.body.user_id;
+  
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+    dbConn.query('DELETE FROM users WHERE id = ?', [user_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'User has been updated successfully.' });
+    });
+}); 
+ 
+// set port
+app.listen(3000, function () {
+    console.log('Node app is running on port 3000');
+});
+ 
+module.exports = app;
